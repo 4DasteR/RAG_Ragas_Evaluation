856ee919e0c2c111f401081d818d1f791f9cf48b
4DasteR <piotrus.grel@gmail.com>
2025-04-11 21:49:52 +0200

    Updated rag_chain to 3 RAG types: SimpleRAG, CompressionRAG and HybridRAG.

--- components/rag_chain.py
+++ components/rag_chain.py
@@ -1,15 +1,23 @@
 from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA, LLMChain
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain.retrievers import EnsembleRetriever
+from langchain_community.retrievers import BM25Retriever
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
+from components.vector_store import load_and_split_documents
+from typing import List
 
 DEFAULT_RETRIEVER_K = 4
 
 
-class RAG:
-    def __init__(self, vectorstore, model, k=DEFAULT_RETRIEVER_K):
+class SimpleRAG:
+    def __init__(self, vectorstore: FAISS | None, model: ChatOpenAI, k=DEFAULT_RETRIEVER_K):
         self.vectorstore = vectorstore
         self.llm = model
-        # Create a retriever
-        retriever = vectorstore.as_retriever(search_kwargs={"k": k})
+        self.k = k
+        self.retriever = self.build_retriever()
 
         # Custom prompt template for RAG
         rag_prompt_template = """
@@ -32,12 +40,19 @@ class RAG:
         self.chain = RetrievalQA.from_chain_type(
             llm=self.llm,
             chain_type="stuff",
-            retriever=retriever,
+            retriever=self.retriever,
             chain_type_kwargs={"prompt": RAG_PROMPT},
             return_source_documents=True
         )
 
-    def query(self, query):
+    def build_retriever(self):
+        if not self.vectorstore:
+            raise ValueError("No vectorstore provided for base retriever.")
+        return self.vectorstore.as_retriever(search_kwargs={"k": self.k})
+
+    def query(self, query: str):
+        if len(query.strip()) == 0 or type(query) is not str:
+            raise ValueError("Query must be provided as a nonempty string.")
         """Query the RAG system with a question"""
         result = self.chain.invoke(query)
 
@@ -46,3 +61,27 @@ class RAG:
             "answer": result["result"],
             "source_documents": result["source_documents"]
         }
+
+
+class CompressionRAG(SimpleRAG):
+    def build_retriever(self):
+        base_retriever = super().build_retriever()
+        compressor = LLMChainExtractor.from_llm(self.llm)
+        return ContextualCompressionRetriever(base_compressor=compressor, base_retriever=base_retriever)
+
+
+class HybridRAG(SimpleRAG):
+    def __init__(self, dense_vectorstore: FAISS, model: ChatOpenAI, documents_paths: List[str], k=DEFAULT_RETRIEVER_K):
+        self.dense_vectorstore = dense_vectorstore
+        self.documents = load_and_split_documents(documents_paths)
+        super().__init__(vectorstore=None, model=model, k=k)
+
+    def build_retriever(self):
+        dense_retriever = self.dense_vectorstore.as_retriever(search_kwargs={"k": self.k})
+        sparse_retriever = BM25Retriever.from_documents(self.documents)
+        sparse_retriever.k = self.k
+
+        return EnsembleRetriever(
+            retrievers=[dense_retriever, sparse_retriever],
+            weights=[0.5, 0.5]
+        )
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from components import openai_model, vector_store
-from components.rag_chain import RAG
+from components.rag_chain import SimpleRAG, CompressionRAG, HybridRAG
 
 if __name__ == "__main__":
+    documents = ['RAG_data\Ethics-Toolkit.pdf']
     # Start Rag pipeline
     llm, embedding_engine = openai_model.provide()
-    vectorstore = vector_store.create(['G:\Studia\TEG\TEG_RAG_EVAL\RAG_data\Ethics-Toolkit.pdf'], embedding_engine)
-    rag = RAG(vectorstore, llm)
+    vectorstore = vector_store.create(documents, embedding_engine)
+    rag = SimpleRAG(vectorstore, llm)
+    # rag = CompressionRAG(vectorstore, llm)
+    # rag = HybridRAG(vectorstore, llm, documents)
 
     test_query = "What are tools of ethics? Enumerate and explain briefly all of them."
     result = rag.query(test_query)
